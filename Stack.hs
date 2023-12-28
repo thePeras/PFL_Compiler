75cf106ec2156ce0ed3e76fb2db90c4743805e02
Madalena Ye <mads1040@gmail.com>
2023-12-28 16:36:07 +0000

    stack accepting boolean and ints

--- Stack.hs
+++ Stack.hs
@@ -1,44 +1,52 @@
-module Stack (Stack, push, pop, top, top1, top2, createEmptyStack, isEmpty, stack2Str) where
+module Stack (Stack, createEmptyStack, push, pop, top, top1, top2, isEmpty, stack2Str) where
 
-data Stack = Stk [Integer]
+-- Definition of the StackElement type
+data StackElement = StackElement Integer | StackElementBool Bool
+  deriving (Show, Eq)
 
-push :: Integer -> Stack -> Stack
-push x (Stk xs) = Stk (x:xs)
+-- Definition of the Stack type
+data Stack = Stack [StackElement]
+  deriving (Show, Eq)
+
+
+createEmptyStack :: Stack
+createEmptyStack = Stack []
+
+push :: StackElement -> Stack -> Stack
+push x (Stack xs) = Stack (x:xs)
 
 pop :: Stack -> Stack
-pop (Stk (_:xs)) = Stk xs
+pop (Stack (_:xs)) = Stack xs
 pop _ = error "Stack.pop: empty stack"
 
-top :: Stack -> Integer
-top (Stk (x:_)) = x
+top :: Stack -> StackElement
+top (Stack (x:_)) = x
 top _ = error "Stack.top: empty stack"
 
 -- Returns a tuple (x, stackWithoutX)
 --  x: top of the stack
 --  stackWithoutX: stack without the top element
-top1 :: Stack -> (Integer, Stack)
+top1 :: Stack -> (StackElement, Stack)
 top1 stack = (x, stackWithoutX)
   where x = top stack
         stackWithoutX = pop stack
+
 
 -- Returns a tuple (x, y, stackWithoutXY)
 --  x: top of the stack
 --  y: second element of the stack
 --  stackWithoutXY: stack without the top two elements
-top2 :: Stack -> (Integer, Integer, Stack)
+top2 :: Stack -> (StackElement, StackElement, Stack)
 top2 stack = (x, y, stackWithoutXY)
   where x = top stack
         y = top (pop stack)
         stackWithoutXY = pop (pop stack)
 
-createEmptyStack :: Stack
-createEmptyStack = Stk []
-
 isEmpty :: Stack -> Bool
-isEmpty (Stk []) = True
-isEmpty (Stk _) = False
+isEmpty (Stack []) = True
+isEmpty (Stack _) = False
 
 stack2Str :: Stack -> String
-stack2Str (Stk []) = ""
-stack2Str (Stk [x]) = show x
-stack2Str (Stk (x:xs)) = show x ++ "," ++ stack2Str (Stk xs)
+stack2Str (Stack []) = ""
+stack2Str (Stack [x]) = show x
+stack2Str (Stack (x:xs)) = show x ++ "," ++ stack2Str (Stack xs)
